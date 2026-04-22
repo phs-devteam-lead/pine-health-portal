43c2d162081fd5da59808db29536e685b10d6a2f
BenMooreNBCC <159150120+BenMooreNBCC@users.noreply.github.com>
2026-04-22 08:57:48 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+    api_url: "https://api.pinehealth.ca/v1",
+    db_host: "ph-db01.internal.local",
+    db_user: "ph_app",
+    db_pass: "PineApp2025!",
+    jwt_secret: "phportal_jwt_secret_key",
+    smtp_server: "smtp.pinehealth.ca",
+    smtp_user: "alerts@pinehealth.ca",
+    smtp_pass: "AlertMail2025!"
+};
+
+module.exports = config;
